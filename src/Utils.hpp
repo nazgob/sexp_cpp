b9738e9e93942dc47b2c91aff254ce980a423818
nazgob <nazgob@gmail.com>
2011-01-18 15:17:22 +0100

    removed dead code

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -6,24 +6,20 @@
 namespace sexp_cpp
 {
   class Exp;
-  class Operator;
   class SymbolExp;
   class ValExp;
   class BoolExp;
   class NullExp;
   class EmptyListExp;
   class PairExp;
-  class SExp;
 
   typedef boost::shared_ptr<Exp> pExp;
-  typedef boost::shared_ptr<Operator> pOp;
   typedef boost::shared_ptr<SymbolExp> pSymbol;
   typedef boost::shared_ptr<ValExp> pVal;
   typedef boost::shared_ptr<NullExp> pNull;
   typedef boost::shared_ptr<BoolExp> pBool;
   typedef boost::shared_ptr<EmptyListExp> pEList;
   typedef boost::shared_ptr<PairExp> pPair;
-  typedef boost::shared_ptr<SExp> pSExp;
 
 } // sexp_cpp
 
